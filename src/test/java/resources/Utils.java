da425853a00c14aa1d63769439d0502f6253a174
SR110292 <suryafromindia@gmail.com>
2023-05-09 21:42:42 +0200

    dummy changes

--- src/test/java/resources/Utils.java
+++ src/test/java/resources/Utils.java
@@ -15,6 +15,7 @@ import java.io.PrintStream;
 import java.util.Properties;
 
 public class Utils {
+    private static RequestSpecification requestSpecification;
 
     public static String getConfigFileValue(String key) throws IOException {
         Properties properties = new Properties();
@@ -26,17 +27,19 @@ public class Utils {
     public RequestSpecification requestSpecification(String deviceType, String tenant) throws IOException {
         DeviceTypeResources deviceTypeResources = DeviceTypeResources.valueOf(deviceType);
         TenantResources tenantResources = TenantResources.valueOf(tenant);
-        PrintStream log = new PrintStream(new FileOutputStream("logs/log.txt"));
-        RequestSpecification requestSpecification = new RequestSpecBuilder()
-                .setBaseUri(getConfigFileValue("baseUrl"))
-                .setContentType(ContentType.JSON)
-                .addHeader("Sae-Device-Type", deviceTypeResources.getDeviceType())
-                .addPathParam("tenant", tenantResources.getTenant())
-                .addFilter(RequestLoggingFilter.logRequestTo(log))
-                .addFilter(ResponseLoggingFilter.logResponseTo(log))
-                .build()
-                .log()
-                .all();
+        if (requestSpecification == null) {
+            PrintStream log = new PrintStream(new FileOutputStream("logs/log.txt"));
+            requestSpecification = new RequestSpecBuilder()
+                    .setBaseUri(getConfigFileValue("baseUrl"))
+                    .setContentType(ContentType.JSON)
+                    .addHeader("Sae-Device-Type", deviceTypeResources.getDeviceType())
+                    .addPathParam("tenant", tenantResources.getTenant())
+                    .addFilter(RequestLoggingFilter.logRequestTo(log))
+                    .addFilter(ResponseLoggingFilter.logResponseTo(log))
+                    .build()
+                    .log()
+                    .all();
+        }
         return requestSpecification;
     }
 
